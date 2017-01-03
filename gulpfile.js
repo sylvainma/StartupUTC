a540b2b6a401d1801fd3ad1121e4cb3338dd8d65
Sylvain M <sylvain.marchienne@gmail.com>
2017-01-03 11:24:01 +0100

    Suppression des fichiers front

--- gulpfile.js
+++ gulpfile.js
@@ -21,102 +21,15 @@ gulp.task('back_without_vendor', function() {
 
 });
 
-gulp.task('front_assets_vendor', function() {
-
-	/*
-	 *	jQuery
-	 */
-
-	gulp.src(front + '/node_modules/jquery/dist/jquery.min.js')
-	.pipe(gulp.dest(dist_front+'/assets/vendor/jquery'))
-
-	/*
-	 *	Bootstrap
-	 */
-
-	gulp.src(front + '/node_modules/bootstrap/dist/css/bootstrap.min.css')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/bootstrap/css'))
-
-	gulp.src(front + '/node_modules/bootstrap/dist/js/bootstrap.min.js')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/bootstrap/js'))
-
-	gulp.src(front + '/node_modules/bootstrap/dist/fonts/*')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/bootstrap/fonts'))
-
-	/*
-	 *	Bootstrap Material
-	 */
-
-	gulp.src(front + '/node_modules/bootstrap-material-design/dist/css/bootstrap-material-design.min.css')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/bootstrap-material-design/css'))
-
-	gulp.src(front + '/node_modules/bootstrap-material-design/dist/css/ripples.min.css')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/bootstrap-material-design/css'))
-
-	gulp.src(front + '/node_modules/bootstrap-material-design/dist/js/material.min.js')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/bootstrap-material-design/js'))
-
-	gulp.src(front + '/node_modules/bootstrap-material-design/dist/js/ripples.min.js')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/bootstrap-material-design/js'))
-
-	/*
-	 *	SigmaJS
-	 */
-
-	gulp.src(front + '/assets/vendor/sigmajs/**/*.js')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/sigmajs'))
-
-	/*
-	 *	noUISlider
-	 */
-
-	gulp.src(front + '/assets/vendor/nouislider/**/*.js')
-  .pipe(gulp.dest(dist_front+'/assets/vendor/nouislider'))
-
-});
-
-gulp.task('front_app_vendor', function() {
-
-	/*
-	 *	AngularJS
-	 */
-
-	var angular = [
-		front + '/node_modules/angular/angular.min.js',
-		front + '/node_modules/angular-resource/angular-resource.min.js',
-		front + '/node_modules/angular-route/angular-route.min.js',
-		front + '/app/vendor/ui-bootstrap-2.2.0.min.js',
-	];
-
-	gulp.src(angular)
-	.pipe(concat('angular.min.js'))
-  .pipe(gulp.dest(dist_front + '/app/vendor'))
-
-});
-
-gulp.task('front', ['front_assets_vendor', 'front_app_vendor'], function() {
+gulp.task('front', function() {
 
 	/*
 	 *	Index.html
 	 */
 
   gulp.src(front + '/index.html')
-  .pipe(concat('app.blade.php'))
+  .pipe(concat('app.php'))
   .pipe(gulp.dest('dist/resources/views'))
-
-	gulp.src(front + '/app/**/*.html')
-  .pipe(gulp.dest('dist/public/app'))
-
-	gulp.src([front + '/app/env.js', front + '/app/app.js', front + '/app/**/*.js'])
-	.pipe(concat('app.js'))
-  .pipe(gulp.dest(dist_front + '/app'))
-
-	/*
-	 *	CSS
-	 */
-
-	gulp.src(front + '/assets/css/*')
-	.pipe(gulp.dest(dist_front+'/assets/css'))
 
 });
 
--- src/front/app/services/StartupsFactory.js
+++
@@ -1,8 +0,0 @@
-/**
- *  Pour les ressources Startups
- */
- app.factory('Startups', function($resource){
-  return $resource(__ENV.apiUrl + "/startups/:id", {}, {
-       'update': { method:'PUT' }
-   });
- });
